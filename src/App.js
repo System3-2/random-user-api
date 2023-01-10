d915a73c532a2721053e509b32dfa9f8bf5e8e62
Oloja <oloja@kali.kali>
2023-01-10 11:46:57 +0100

    Recent changes added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { useReducer } from "react";
 
+const initialState = {
+  count: 5,
+  isDark: false,
+};
 const ACTION = {
   INCREMENT: "increment",
   DECREMENT: "decrement",
-  DARKMODE: 'darkmode',
-  LIGHTMODE: 'lightmode'
+  DARKMODE: "darkmode",
+  LIGHTMODE: "lightmode",
 };
 const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case ACTION.INCREMENT:
-      return {count: state.count + 1};
+      return { ...state, count: state.count + 1 };
 
-      case ACTION.DECREMENT:
-        return {count: state.count - 1};
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 };
 
-        case ACTION.DARKMODE:
-          return { isDark: state.isDark = !state.isDark }
-        default:
-          throw new Error();
+    case ACTION.DARKMODE:
+      return { ...state, isDark: !state.isDark };
+    default:
+      throw new Error();
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 5, isDark: false });
-  console.log(state.isDark);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function increment() {
+    dispatch({ type: ACTION.INCREMENT });
+  }
+  function decrement() {
+    dispatch({ type: ACTION.DECREMENT });
+  }
+  function toggleDarkMode() {
+    dispatch({ type: ACTION.DARKMODE });
+    console.log(state.isDark);
+  }
 
   return (
     <>
-      <h1>Count is {state.count}</h1>
-      <button onClick={() => dispatch({type: ACTION.INCREMENT})}>Increment</button>
-      <button onClick={() => dispatch({type: ACTION.DECREMENT})}>Decrement</button>
-      <button onClick={() => dispatch({type: ACTION.DARKMODE})}>Dark</button>
+      <div className={state.isDark ? "dark" : "light"}>
+        <h1>Count is {state.count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={toggleDarkMode}>Dark</button>
+      </div>
     </>
   );
 }
