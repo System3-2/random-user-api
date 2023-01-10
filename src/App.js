fd3b8f2e98e5cb9749ef4d63c7ae1211cd653243
Oloja <oloja@kali.kali>
2023-01-10 10:36:35 +0100

    useReducer branch added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  DARKMODE: 'darkmode',
+  LIGHTMODE: 'lightmode'
+};
+const reducer = (state, action) => {
+  switch(action.type) {
+    case ACTION.INCREMENT:
+      return {count: state.count + 1};
+
+      case ACTION.DECREMENT:
+        return {count: state.count - 1};
+
+        case ACTION.DARKMODE:
+          return { isDark: state.isDark = !state.isDark }
+        default:
+          throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 5, isDark: false });
+  console.log(state.isDark);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Count is {state.count}</h1>
+      <button onClick={() => dispatch({type: ACTION.INCREMENT})}>Increment</button>
+      <button onClick={() => dispatch({type: ACTION.DECREMENT})}>Decrement</button>
+      <button onClick={() => dispatch({type: ACTION.DARKMODE})}>Dark</button>
+    </>
   );
 }
 
